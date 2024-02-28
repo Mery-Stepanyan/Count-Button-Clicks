a64823b2cf35c966482787c8bec460ed24b7e0d5
Mery-Stepanyan <64136735+Mery-Stepanyan@users.noreply.github.com>
2024-02-28 22:47:49 +0400

    -
    -

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const button = document.getElementById('clickButton');
+  const clickCountDisplay = document.getElementById('clickCount');
+  let clickCount = 0;
+
+  button.addEventListener('click', function() {
+    clickCount++;
+    clickCountDisplay.value = clickCount;
+  });
+});
